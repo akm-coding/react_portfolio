7d2cc7d580309ba9582b83239ef61ba962defca2
naylynnmyat9504 <aungkaungmyatyer123@gmail.com>
2023-05-17 12:05:23 +0630

    second changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -103,7 +103,7 @@ const Navbar = () => {
           <li className=" w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
             <a
               className=" flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href="https://github.com/aungkaungmyat9504"
             >
               Github <FaGithub size={30} />
             </a>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -38,7 +38,7 @@ const Work = () => {
                       className="text-center rounded-lg px-4 py-3 m-2
                        bg-white text-gray-700 font-bold text-lg"
                     >
-                      Code
+                      Demo
                     </button>
                   </a>
                   {/* eslint-disable-next-line */}
